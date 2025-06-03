028169a1d2129a1c469b2bc12c434fddc7a814b3
ismagpt <ismael.alvrdz@hotmail.com>
2025-06-03 17:32:26 -0600

    Fix puerto para Render

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def whatsapp_reply():
     return str(response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
